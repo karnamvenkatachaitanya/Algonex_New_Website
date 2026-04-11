d366bed60021772f8d38dcc32cd47f49e2cf7b10
saikumar-pappaka <104044890+mrsaikumar-7@users.noreply.github.com>
2026-04-11 13:50:40 +0530

    feat(careers): add apply_mode filter to JobFilter

--- algonex-backend/careers/filters.py
+++ algonex-backend/careers/filters.py
@@ -7,7 +7,8 @@ class JobFilter(django_filters.FilterSet):
     job_type = django_filters.ChoiceFilter(choices=Job.JOB_TYPE_CHOICES)
     is_remote = django_filters.BooleanFilter()
     search = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
+    apply_mode = django_filters.ChoiceFilter(choices=Job.APPLY_MODE_CHOICES)
 
     class Meta:
         model = Job
-        fields = ["department", "job_type", "is_remote"]
+        fields = ["department", "job_type", "is_remote", "apply_mode"]
